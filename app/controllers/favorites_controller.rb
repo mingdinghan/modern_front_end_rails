900a05dbbad44c2e39bd1f08357a2dd8a06a89b6
Mingding Han <mingdinghan@gmail.com>
2022-08-27 22:16:42 +0800

    Use Turbo Streams to update multiple parts of a page from a request

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -11,14 +11,18 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    Favorite.create(user: current_user, concert_id: params[:concert_id])
-    render(partial: "favorites/list")
+    @favorite = Favorite.create(user: current_user, concert_id: params[:concert_id])
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
-    render(partial: "favorites/list")
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   private def favorite_params
